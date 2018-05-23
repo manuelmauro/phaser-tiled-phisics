9ed59ef8d342055b993ebfeba8fee24621b2b090
Gedl <gedl@protonmail.com>
2018-05-23 12:53:41 +0200

    Rename tilemap property 'friction' to 'inertia'

--- src/modifiers/Inertia.js
+++ src/modifiers/Inertia.js
@@ -7,7 +7,7 @@
 /**
  * @typedef {object} Tile
  *
- * @property {float} [friction] - [description]
+ * @property {float} [inertia] - [description]
  *
  */
 class Inertia {
@@ -20,15 +20,6 @@ class Inertia {
      * @since 0.1.0
      */
     this.body = body;
-
-    /**
-     * [description]
-     *
-     * @name
-     * @type
-     * @since 0.1.0
-     */
-    this.body.sliding = false;
 
     /**
      * [description]
@@ -66,11 +57,11 @@ class Inertia {
     const id = this.layer.data[tile.tx][tile.ty];
     const props = this.layer.tileset.get(id) || {};
 
-    if (!props.friction) { props.friction = 0; }
-    this.body.velocity.x = props.friction * this.body.velocity.x;
-    this.body.velocity.y = props.friction * this.body.velocity.y;
-    this.body.acceleration.x = props.friction * this.body.acceleration.x;
-    this.body.acceleration.y = props.friction * this.body.acceleration.y;
+    if (!props.inertia) { props.inertia = 0; }
+    this.body.velocity.x = props.inertia * this.body.velocity.x;
+    this.body.velocity.y = props.inertia * this.body.velocity.y;
+    this.body.acceleration.x = props.inertia * this.body.acceleration.x;
+    this.body.acceleration.y = props.inertia * this.body.acceleration.y;
 
     this.body.sliding = props.friction > 0;
   }
